b9328a555ff7814e85c7435b8abaa26bcb4566f3
Hau Do <haudokv@gmail.com>
2018-08-01 18:44:40 +0700

    MMSB-2 Insert an empty spring-boot project up to this repo. This project declared some dependency-libraries in pom.xml

---
+++ src/main/java/com/hdo/multi/modules/MultiModulesApplication.java
@@ -0,0 +1,12 @@
+package com.hdo.multi.modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModulesApplication.class, args);
+	}
+}
